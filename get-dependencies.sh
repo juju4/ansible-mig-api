e8905da724c394bbe5058fe6a7de6123cabdf146
juju4 <juju4@users.noreply.github.com>
2018-10-27 10:49:37 -0400

    add missing role dependency

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -9,6 +9,7 @@ rolesdir=$(dirname $0)/..
 [ ! -d $rolesdir/juju4.gpgkey_generate ] && git clone https://github.com/juju4/ansible-gpgkey_generate $rolesdir/juju4.gpgkey_generate
 [ ! -d $rolesdir/juju4.redhat-epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat-epel
 [ ! -d $rolesdir/juju4.golang ] && git clone https://github.com/juju4/ansible-golang $rolesdir/juju4.golang
+[ ! -d $rolesdir/juju4.mig-postgres ] && git clone https://github.com/juju4/ansible-mig-postgres $rolesdir/juju4.mig-postgres
 ## galaxy naming: kitchen fails to transfer symlink folder
 #[ ! -e $rolesdir/juju4.mig-api ] && ln -s ansible-mig-api $rolesdir/juju4.mig-api
 [ ! -e $rolesdir/juju4.mig-api ] && cp -R $rolesdir/ansible-mig-api $rolesdir/juju4.mig-api

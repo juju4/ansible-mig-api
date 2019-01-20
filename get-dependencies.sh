aaf39b2388e680e824bd4aaf51b7629aeec37d85
juju4 <juju4@users.noreply.github.com>
2019-01-20 09:23:35 -0500

    update galaxy naming for dependency

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -7,7 +7,7 @@ rolesdir=$(dirname $0)/..
 
 [ ! -d $rolesdir/juju4.adduser ] && git clone https://github.com/juju4/ansible-adduser $rolesdir/juju4.adduser
 [ ! -d $rolesdir/juju4.gpgkey_generate ] && git clone https://github.com/juju4/ansible-gpgkey_generate $rolesdir/juju4.gpgkey_generate
-[ ! -d $rolesdir/juju4.redhat-epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat-epel
+[ ! -d $rolesdir/juju4.redhat_epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat_epel
 [ ! -d $rolesdir/juju4.golang ] && git clone https://github.com/juju4/ansible-golang $rolesdir/juju4.golang
 [ ! -d $rolesdir/juju4.mig-postgres ] && git clone https://github.com/juju4/ansible-mig-postgres $rolesdir/juju4.mig-postgres
 ## galaxy naming: kitchen fails to transfer symlink folder
